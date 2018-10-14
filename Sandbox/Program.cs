641fce6ef1dc16b041eb034be0a8b3b66b6ecba5
Mike <Mike@MIKE-ZENBOOK>
2018-10-14 15:46:27 +1030

    Test upload

---
+++ Sandbox/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new WebClient())
+            {
+                client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+                client.DownloadFile("http://www.bom.gov.au/fwo/aviation/IDY02749.pdf", @"C:\Users\mike_\Downloads\test.pdf");
+            }
+        }
+    }
+}
